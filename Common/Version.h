fb8755f6456de17a5315dd2961910b2b91076a06
Martin Dubois <mdubois@kms-quebec.com>
2026-05-28 07:47:09 -0400

    Version and resources

---
+++ Common/Version.h
@@ -0,0 +1,18 @@
+
+// Author    KMS - Martin Dubois, P. Eng.
+// Copyright (C) 2026 KMS
+// License   http://www.apache.org/licenses/LICENSE-2.0
+// Product   TLBoDP
+// File      Common/Version.h
+
+#pragma  once
+
+// Constantes
+/////////////////////////////////////////////////////////////////////////////
+
+#define VERSION_RC   0,0,0,0
+#define VERSION_STR  "0.0.0.0"
+
+#if 0
+    KMS_VERSION("dev")
+#endif
